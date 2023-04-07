8d8c8bed9d823afa864a4a5e8a6fad4e2afaca40
Rihhard Elm <elm.rihhard@gmail.com>
2023-04-07 22:20:51 +0300

    room change functionality rehashing

--- chat/urls.py
+++ chat/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('api/register_user/', views.register_user, name="register_user"),
     path('api/login_user/', views.login_user, name="login_user"),
     path('api/log_out_user/', views.logout_user, name="logout_user"),
-    path("chat/room=<str:room_name>/", views.room, name="room")
+    path("chat/room=<str:room_name>/", views.room, name="room"),
+    path('api/change_room/', views.change_room, name="change_room"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,4 @@
-import json
-import os 
+from .manageroomdata import *
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.models import User
@@ -26,6 +25,7 @@ def register_user(request):
         if User.objects.filter(username=username).exists():
             status_code = 409
             return JsonResponse({'error': 'Username already exists.'}, status=status_code)
+        
         user = User.objects.create_user(username=username, password=password)
         user.save()
         return JsonResponse({'success': True, 'username': username})
@@ -37,8 +37,10 @@ def login_user(request):
     username = request.data['username']
     password = request.data['password']
     room = request.data['room']
+
     if username and password:
         user = authenticate(username=username, password=password)
+
         if user is not None:
             login(request, user)
             save_as_json(username, room)
@@ -48,48 +50,30 @@ def login_user(request):
             return JsonResponse({'error': 'Invalid username or password'}, status=status_code)
     return JsonResponse({'error': 'Invalid request'})
 
+
 @api_view(['POST'])
 def logout_user(request):
     room = request.data['room']
     username = request.data['username']
+
     logout(request)
     delete_from_json(username, room)
+
     return JsonResponse({'success': True})
 
 
-def save_as_json(username, roomname):
-    json_file_name = './static/js/roomUsers.json'
-    room_object = {
-       'roomname': roomname, 
-       'users': [username]
-       }
-    with open(json_file_name, 'r') as f:
-      json_data = json.load(f)
+@api_view(['POST'])
+def change_room(request):
+    room = request.data['room']
+    username = request.data['username']
+    req_type = request.data['type']
+
+    if req_type == 'change_room':
+      new_room = request.data['new_room']
+      delete_from_json(username, room)
+      save_as_json(username, new_room)
+      return JsonResponse({'success': True, 'response': 'changing room'})
     
-    for room in json_data:
-      if room['roomname'] == roomname:
-            if username not in room['users']:
-              room['users'].append(username)
-            dump_to_json(json_file_name, json_data)
-            return
-    json_data.append(room_object)
-    dump_to_json(json_file_name, json_data)
-    return
-
-def delete_from_json(username, roomname):
-    json_file_name = './static/js/roomUsers.json'
-
-    with open(json_file_name, 'r') as f:
-      json_data = json.load(f)
-    
-    for room in json_data:
-      if room['roomname'] == roomname:
-        for user in room['users']:
-          if user == username:
-            room['users'].remove(username)
-    dump_to_json(json_file_name, json_data)
-    return
-
-def dump_to_json(json_file_name, json_data):
-    with open(json_file_name, 'w') as f:
-      json.dump(json_data, f, indent=2)
+    elif req_type == 'leave_room':
+      delete_from_json(username, room)
+      return JsonResponse({'success': True, 'response': 'deleted user'})
