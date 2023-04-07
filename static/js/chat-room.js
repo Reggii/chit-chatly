b2bfb8fe1eee5ebd64d9f834d8805874f5c8bf21
Rihhard Elm <elm.rihhard@gmail.com>
2023-04-07 22:21:13 +0300

    client side room change functionality

--- static/js/chat-room.js
+++ static/js/chat-room.js
@@ -1,6 +1,7 @@
 import '../alertifyjs/alertify.js'
 
 const roomName = JSON.parse(document.getElementById('room-name').textContent);
+const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 const userName = document.cookie
   .split("; ")
   .find((row) => row.startsWith("username="))
@@ -29,7 +30,12 @@ chatSocket.onmessage = function(e) {
 chatSocket.addEventListener('close', function (event) {
     alertify.alert('Connection closed', 'You have been disconnected from the server').set(
         'onok', function(closeEvent){ 
-            window.location.href = `http://127.0.0.1:8000/`;
+            const sendData = {
+                'username': userName,
+                'room': roomName,
+                'type': 'leave_room'
+            }
+            change_room(sendData)
         })
     console.log('disconnected');
 });
@@ -56,7 +62,6 @@ document.querySelector('#chat-message-submit').onclick = function(e) {
 
 
 document.querySelector('#log-out').onclick = function(e) {
-    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
     const sendData ={
         'username': userName,
         'room': roomName
@@ -83,11 +88,11 @@ document.querySelector('#log-out').onclick = function(e) {
 
 
 document.querySelector('#change-room').onclick = function(e) {
-    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-    alertify.prompt('Change room', 'Enter the room you want to join', function(evt, value) {
+
+    alertify.prompt('Change room', 'Enter the room you want to join', function(evt, newRoom) {
     const alertifyHeader = document.querySelector('.ajs-header')
     const alertifyCommands = document.querySelector('.ajs-close')
-    if (value.length < 3 || value.length > 12) {
+    if (newRoom.length < 3 || newRoom.length > 12) {
             alertify.alert('Invalid room name', 'Room name can be between 3 and 10 characters long')
             alertifyCommands.style.display = 'none'
             alertifyHeader.style.padding = '0px'
@@ -96,28 +101,90 @@ document.querySelector('#change-room').onclick = function(e) {
             alertifyHeader.style.color = '#1e1e1e'
             return
         } 
+
     else {  
-    $.ajax({
-        url: '/chat/room=' + value + '/',
-        method: 'POST',
-        data: JSON.stringify(userName),
-        headers: {
-            "Content-Type": "application/json",
-            'X-CSRFToken': csrftoken
-        },
-        success: function(data) {
-            console.log(`Connected to room ${value}`)
-            document.cookie = `username=${userName}`
-            window.location.href = `/chat/room=${value}`
-        },
-        error: function(data) {
-            console.log(data);
-            alertify.alert('Error', 'Username or password incorrect')
+    const sendData ={
+        'username': userName,
+        'room': roomName,
+        'new_room': newRoom,
+        'type': 'change_room',
         }
-    }); }
+    change_room(sendData)
+        }
     });
 };
- 
+
+function change_room(sendData) {
+    $.ajax({
+    url: '/api/change_room/',
+    method: 'POST',
+    data: JSON.stringify(sendData),
+    headers: {
+        "Content-Type": "application/json",
+        'X-CSRFToken': csrftoken
+    },
+    success: function(data) {
+        if (data.response == 'changing room') {
+            document.cookie = `username=${userName}`
+            window.location.href = `/chat/room=${sendData.new_room}`
+             }
+        else if (data.response == 'deleted user') {
+            window.location.href = `http://127.0.0.1:8000/`
+            }
+    },
+    error: function(data) {
+        console.log(data);
+        alertify.alert('Error', 'Username or password incorrect')
+    }
+    })}
+
+// function delete_from_json() {
+//     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+//     const sendData ={
+//         'username': userName,
+//         'room': roomName
+//     }
+//     $.ajax({
+//         url: '/api/delete_user_from_room/',
+//         method: 'POST',
+//         data: JSON.stringify(sendData),
+//         headers: {
+//             "Content-Type": "application/json",
+//             'X-CSRFToken': csrftoken
+//         },
+//         success: function(data) {
+//             console.log(data);
+//         },
+//         error: function(data) {
+//             console.log(data);
+//         }
+//     });
+//   }
+
+// function save_to_json() {
+//     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+//     const sendData ={
+//         'username': userName,
+//         'room': roomName
+//     }
+//     $.ajax({
+//         url: '/api/save_user_to_room/',
+//         method: 'POST',
+//         data: JSON.stringify(sendData),
+//         headers: {
+//             "Content-Type": "application/json",
+//             'X-CSRFToken': csrftoken
+//         },
+//         success: function(data) {
+//             console.log(data);
+//         },
+//         error: function(data) {
+//             console.log(data);
+//         }
+//     });
+//   }
+
+
 const roomHeader = document.querySelector('#room_name')
 roomHeader.innerHTML = roomHeader.innerHTML + roomName
 
