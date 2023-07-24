5d6844259cc08f64b99dc80d9b51cff3c40156d6
Rihhard Elm <elm.rihhard@gmail.com>
2023-07-24 21:22:32 +0300

    settings config

--- chatsite/settings.py
+++ chatsite/settings.py
@@ -26,7 +26,7 @@ DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
