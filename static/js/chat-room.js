639bc097ffffc614dc2c1a96671903418d7c5fc0
Rihhard Elm <elm.rihhard@gmail.com>
2023-08-06 18:39:34 +0300

    add a log

--- static/js/chat-room.js
+++ static/js/chat-room.js
@@ -4,6 +4,7 @@ let onlineUsers = []
 const roomName = JSON.parse(document.getElementById('room-name').textContent);
 const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 const userName = document.cookie
+console.log(userName)
   .split("; ")
   .find((row) => row.startsWith("username="))
   ?.split("=")[1];
